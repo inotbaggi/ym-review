61477e6dd184bd14413a2471abed24e3c303a78c
inotbaggi <inotbaggi@gmail.com>
2024-11-29 15:27:59 +0300

    Initial commit

---
+++ src/main/kotlin/me/baggi/ymreview/YmReviewApplication.kt
@@ -0,0 +1,11 @@
+package me.baggi.ymreview
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class YmReviewApplication
+
+fun main(args: Array<String>) {
+    runApplication<YmReviewApplication>(*args)
+}
---
+++ src/test/kotlin/me/baggi/ymreview/YmReviewApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.baggi.ymreview
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class YmReviewApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
